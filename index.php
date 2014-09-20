225de95ed087c49956cade8e16bde0004c1c0c15
yosketch <yosketch@gmail.com>
2014-09-20 15:16:11 +0900

    Add basic HTML5 tags

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>easy2read4me</title>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--[if lt IE 9]>
+<script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- <link rel="stylesheet" href=""> -->
+<!-- <link rel="shortcut icon" href=""> -->
+</head>
+<body>
+<h1>easy2read4me</h1>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</body>
+</html>
+
